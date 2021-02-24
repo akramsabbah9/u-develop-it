ad767e6b677e3c419ce1ddc1b2427fb3325c710f
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-24 14:25:58 -0800

    added post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
+const inputCheck = require("./utils/inputCheck");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -68,16 +69,28 @@ app.delete("/api/candidate/:id", (req, res) => {
 });
 
 // create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                 VALUES (?,?,?,?)`;
-// const params = [1, "Ronald", "Firbank", 1];
+app.post("/api/candidate", ({ body }, res) => {
+    const errors = inputCheck(body, "first_name", "last_name", "industry_connected");
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
 
-// db.run(sql, params, function(err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this.lastID);
-// });
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+                VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: body,
+            id: this.lastID
+        });
+    });
+});
 
 // default catch-all response for any other request (Not Found)
 // NEEDS TO BE PLACED AFTER OTHER ROUTES, OR ROUTING WILL PRIORITIZE THIS!
