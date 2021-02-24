727abbfa47eee62279781d9b36524ee865f406db
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-24 14:09:36 -0800

    added get and delete routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,77 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database("./db/election.db", err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log("Connected to the election database.");
+});
+
+// get all candidates
+app.get("/api/candidates", (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
+});
+
+// get a single candidate
+app.get("/api/candidate/:id", (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: row
+        });
+    });
+});
+
+// delete a candidate
+app.delete("/api/candidate/:id", (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "successfully deleted",
+            changes: this.changes
+        });
+    });
+});
+
+// create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+
+// db.run(sql, params, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// });
+
 // default catch-all response for any other request (Not Found)
 // NEEDS TO BE PLACED AFTER OTHER ROUTES, OR ROUTING WILL PRIORITIZE THIS!
 app.use((req, res) => {
@@ -14,6 +86,9 @@ app.use((req, res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}.`);
+// Start server AFTER db connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}.`);
+    });
 });
